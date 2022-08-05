82de26d6b4eaee3550d774bc5c94ff71a0961765
Florian <florian.tellier02@gmail.com>
2022-08-05 22:45:48 +0200

    task style started

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  taskDone: boolean = false;
+  onTaskStatechange(){
+    this.taskDone = !this.taskDone;
+  }
+
+}
